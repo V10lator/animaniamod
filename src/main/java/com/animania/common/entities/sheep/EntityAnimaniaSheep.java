d97a96917e981316b471cec6d40058f13eec8d3d
Thomas Rohloff <v10lator@myway.de>
2018-11-12 14:16:55 +0100

    Simplify GenericAIFind*.shouldExecute()
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main/java/com/animania/common/entities/sheep/EntityAnimaniaSheep.java
+++ src/main/java/com/animania/common/entities/sheep/EntityAnimaniaSheep.java
@@ -115,17 +115,17 @@ public class EntityAnimaniaSheep extends EntitySheep implements IShearable, IAni
 		this.tasks.addTask(1, new GenericAIWanderAvoidWater(this, 1.0D));
 		this.tasks.addTask(2, new EntityAISwimming(this));
 		this.tasks.addTask(3, new GenericAIPanic(this, 2.2D));
-		this.tasks.addTask(5, new GenericAITempt(this, 1.25D, false, EntityAnimaniaSheep.TEMPTATION_ITEMS));
+		this.tasks.addTask(4, new GenericAITempt(this, 1.25D, false, EntityAnimaniaSheep.TEMPTATION_ITEMS));
 		this.tasks.addTask(4, new GenericAITempt(this, 1.25D, Item.getItemFromBlock(Blocks.YELLOW_FLOWER), false));
 		this.tasks.addTask(4, new GenericAITempt(this, 1.25D, Item.getItemFromBlock(Blocks.RED_FLOWER), false));
-		this.tasks.addTask(6, this.entityAIEatGrass);
-		this.tasks.addTask(7, new EntityAIAvoidEntity(this, EntityWolf.class, 24.0F, 2.0D, 2.2D));
-		this.tasks.addTask(8, new GenericAIWatchClosest(this, EntityPlayer.class, 6.0F));
-		this.tasks.addTask(9, new EntityAILookIdle(this));
-		this.tasks.addTask(10, new GenericAIFindSaltLick(this, 1.0, entityAIEatGrass));
+		this.tasks.addTask(5, this.entityAIEatGrass);
+		this.tasks.addTask(6, new EntityAIAvoidEntity(this, EntityWolf.class, 24.0F, 2.0D, 2.2D));
+		this.tasks.addTask(7, new GenericAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		this.tasks.addTask(8, new EntityAILookIdle(this));
+		this.tasks.addTask(9, new GenericAIFindSaltLick(this, 1.0, entityAIEatGrass));
 		if (AnimaniaConfig.gameRules.animalsSleep)
 		{
-			this.tasks.addTask(11, new GenericAISleep<EntityAnimaniaSheep>(this, 0.8, AnimaniaHelper.getBlock(AnimaniaConfig.careAndFeeding.sheepBed), AnimaniaHelper.getBlock(AnimaniaConfig.careAndFeeding.sheepBed2), EntityAnimaniaSheep.class));
+			this.tasks.addTask(10, new GenericAISleep<EntityAnimaniaSheep>(this, 0.8, AnimaniaHelper.getBlock(AnimaniaConfig.careAndFeeding.sheepBed), AnimaniaHelper.getBlock(AnimaniaConfig.careAndFeeding.sheepBed2), EntityAnimaniaSheep.class));
 		}
 		this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, false, new Class[0]));
 		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, EntityPlayer.class));

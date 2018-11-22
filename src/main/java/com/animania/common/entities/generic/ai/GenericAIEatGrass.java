2ee5c6b57e9f02b2eb27ca4a60fb3650b4143dd3
Thomas Rohloff <v10lator@myway.de>
2018-11-22 23:31:13 +0100

    Fix DesirePath compat slippery
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/main/java/com/animania/common/entities/generic/ai/GenericAIEatGrass.java
+++ src/main/java/com/animania/common/entities/generic/ai/GenericAIEatGrass.java
@@ -126,9 +126,7 @@ public class GenericAIEatGrass<T extends EntityCreature & ISleeping & IFoodEatin
 
 					if (AnimaniaConfig.gameRules.plantsRemovedAfterEating)
 					{
-
-						String name = block.getRegistryName().toString();
-						if (Loader.isModLoaded("desirepaths") && ((name.contains("desirepaths:grass_worn_") && (name.endsWith("1") || name.endsWith("2") || name.endsWith("3"))) || block instanceof BlockGrass) )
+						if (Loader.isModLoaded("desirepaths"))
 							this.entityWorld.setBlockState(this.seekingBlockPos, Block.getBlockFromName("desirepaths:grass_worn_6").getDefaultState());
 						else
 							this.entityWorld.setBlockState(this.seekingBlockPos, Blocks.DIRT.getDefaultState(), 2);
